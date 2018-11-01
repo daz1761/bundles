553242a9fbddfefc9585c24c65e702d0b188f9a5
daz1761 <dazgti@btinternet.com>
2018-11-01 20:05:51 +0000

    Project commit

---
+++ BundleFace/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BundleFace
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles); // definition is BundleConfig.cs in App_Start
+
+            // right mofo's this is how you minimise all the CSS bundles in our case
+            System.Web.Optimization.BundleTable.EnableOptimizations = true;
+        }
+    }
+}
